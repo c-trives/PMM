00070c8c8495e59c5b588bd8eb2f685bb0a750a0
c-trives <el_trives@hotmail.es>
2015-11-05 14:27:06 +0100

    ListaSimpleConAdaptador

---
+++ ListaSimple/app/src/main/java/iesserpis/mati/cristian/listasimple/MainActivity.java
@@ -0,0 +1,68 @@
+package iesserpis.mati.cristian.listasimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  ListView listView = (ListView) findViewById(R.id.LstOpciones);
+    //    final TextView texto1 = (TextView) findViewById(R.id.texto1);
+
+        final String[] datos =
+                new String[]{"Elem1","Elem2","Elem3","Elem4","Elem5"};
+
+        ArrayAdapter<String> adaptador =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1, datos);
+
+        final ListView lstOpciones = (ListView)findViewById(R.id.LstOpciones);
+        lstOpciones.setAdapter(adaptador);
+
+        lstOpciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showToast(lstOpciones.getItemAtPosition(position).toString());
+
+            }
+        });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
