3f81ed7806dd8d19a0b786f320d24c55cc74b60a
c-trives <el_trives@hotmail.es>
2015-11-30 12:52:25 +0100

    Subido figuras aleatorias 2 botones

---
+++ FigurasAleatorias/app/src/main/java/iesserpis/mati/cristian/figurasaleatorias/DrawShapes1.java
@@ -0,0 +1,56 @@
+package iesserpis.mati.cristian.figurasaleatorias;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DrawShapes1 extends AppCompatActivity {
+
+    private RandomShapeView mDrawingArea;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_shapes1);
+        mDrawingArea  = (RandomShapeView)findViewById(R.id.drawing_area);
+    }
+
+
+    public void redraw(View clickedButton) {
+        mDrawingArea.invalidate();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_draw_shapes1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
+
+
+
---
+++ FigurasAleatorias/app/src/main/java/iesserpis/mati/cristian/figurasaleatorias/RandomUtils.java
@@ -0,0 +1,32 @@
+package iesserpis.mati.cristian.figurasaleatorias;
+
+import java.util.Random;
+
+/**
+ * Created by mati on 30/11/15.
+ */
+public class RandomUtils {
+
+    private static Random r = new Random();
+
+    /** Devuelve un entero aleatorio en el intervalo [0,range-1]. */
+     public static int randomInt(int range) {
+     return(r.nextInt(range));
+     }
+
+     /** Devuelve un indice aleatorio en el intervalo [0 array.length-1] */
+    public static int randomIndex(Object[] array) {
+        return(randomInt(array.length));
+    }
+
+    /** Devuelve un elemento aleatorio perteneciente a un array
+     * se usa genericidad y siempre necesita un valor de retorno.*/
+    public static <T> T randomElement(T[] array) {
+        return(array[randomIndex(array)]);
+    }
+
+    /** devuelve un numero float aleatorio, en el intervalo[0, n) */
+    public static float randomFloat(int n) {
+        return((float)Math.random()*n);
+    }
+}
